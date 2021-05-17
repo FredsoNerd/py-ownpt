2703f6413eac9d94d5a5326795947d31b3c9bc00
FredsoNerd <fredsonerd@hotmail.com>
2021-05-17 01:11:07 -0300

    review update

--- pyownpt/cli/update_ownpt.py
+++ pyownpt/cli/update_ownpt.py
@@ -1,40 +1,84 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import argparse
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 from json import loads
 from rdflib import Graph
-from pyownpt.update import update_ownpt_from_dump
+from pyownpt.update import Update
 
 
 def _parse(args):
     ownpt_filapath = args.owp
-    wn_filepath = args.wnd
     ownpt_format = args.fmt
+    suggestions_filepath = args.sgs
+    votes_filepath = args.vts
     output_filepath = args.o
     output_format = args.f
+    users_senior = args.u
+    trashold_senior = args.ts
+    trashold_junior = args.tj
+    actions_filepath = args.a
 
-    cli_update_ownpt_from_dump(ownpt_filapath, wn_filepath,
-        ownpt_format, output_filepath, output_format)
+    # sets logging
+    fileHandler = logging.FileHandler(filename="log-update", mode="w")
+    fileHandler.setLevel(logging.DEBUG)
+    streamHandler = logging.StreamHandler(stream=sys.stdout)
+    streamHandler.setLevel(level=30-10*args.v)
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[streamHandler,fileHandler])
+
+    # cals main function
+    cli_update_ownpt_from_dump(ownpt_filapath, suggestions_filepath,
+        votes_filepath, ownpt_format, output_filepath, output_format,
+        users_senior, trashold_senior, trashold_junior, actions_filepath)
+
 
 def cli_update_ownpt_from_dump(
     ownpt_filapath:str,
-    wn_filepath:str,
+    suggestions_filepath:str,
+    votes_filepath:str, 
     ownpt_format:str="nt",
     output_filepath:str="output.xml",
-    output_format:str="xml"):
+    output_format:str="xml",
+    users_senior=[],
+    trashold_senior=1,
+    trashold_junior=2,
+    actions_filepath:str=None):
     """"""
 
-    doc_wn = [loads(line) for line in open(wn_filepath).readlines()]
-    wn = [item["_source"] for item in doc_wn]
+    # loads the data
+    logger.info(f"loading data from '{votes_filepath}'")
+    doc_votes = [loads(line) for line in open(votes_filepath).readlines()]
 
+    logger.info(f"loading data from '{suggestions_filepath}'")
+    doc_suggestions = [loads(line) for line in open(suggestions_filepath).readlines()]
+
+    actions = None
+    if actions_filepath is not None:
+        logger.info(f"loading data from '{actions_filepath}'")
+        actions = loads(open(actions_filepath).read())
+
+    # loading graph
+    logger.info(f"loading data from '{ownpt_filapath}'")
     ownpt = Graph().parse(ownpt_filapath, format=ownpt_format)
-    update_ownpt_from_dump(ownpt, wn)
+    
+    # updates graph
+    update = Update(ownpt)
+
+    if actions is not None:
+        logger.info(f"applying actions from Comparing")
+        update.update_from_compare(actions)
+
+    logger.info(f"applying actions from Suggestions")
+    Update(ownpt).update(doc_suggestions, doc_votes,
+        users_senior, trashold_senior, trashold_junior)
 
     # saves results
-    ownpt.serialize(output_filepath, format=output_format, encoding="utf8")
+    logger.info(f"serializing results to '{output_filepath}'")
+    ownpt.serialize(output_filepath, format=output_format)
 
 
 # sets parser and interface function
@@ -42,10 +86,15 @@ parser = argparse.ArgumentParser()
 
 # sets the user options
 parser.add_argument("owp", help="rdf file from own-pt")
-parser.add_argument("wnd", help="jsonl dump file wn.json")
-parser.add_argument("fmt", help="own-pt rdf format (default: xml)", default="xml")
+parser.add_argument("fmt", help="rdf format (default: xml)", default="xml")
+parser.add_argument("sgs", help="file suggestions.json")
+parser.add_argument("vts", help="file votes.json")
 parser.add_argument("-o", help="output file (default: output.xml)", default="output.xml")
 parser.add_argument("-f", help="output file format (default: xml)", default="xml")
+parser.add_argument("-u", help="list of senior/proficient users", nargs="*", default=[])
+parser.add_argument("-ts", help="senior suggestion score trashold (default: 1)", default=1)
+parser.add_argument("-tj", help="junior suggestion score trashold (default: 2)", default=2)
+parser.add_argument("-a", help="annotated file with actions to be applied", default=None)
 
 parser.add_argument("-v", help="increase verbosity (example: -vv for debugging)", action="count", default=0)
 
--- pyownpt/update.py
+++ pyownpt/update.py
@@ -1,288 +1,190 @@
 # -*- coding: utf-8 -*-
 
 import logging
-logger = logging.getLogger(__name__)
+from typing import AbstractSet
+import tqdm
 
-from rdflib import Graph, Namespace, URIRef, BNode
-from rdflib import Literal, XSD, RDF, RDFS, SKOS, OWL
-
-# global
-OWNPT = Namespace("https://w3id.org/own-pt/wn30/schema/")
-NOMLEX = Namespace("https://w3id.org/own-pt/nomlex/schema/")
-
-WORD = Namespace("https://w3id.org/own-pt/wn30-pt/instances/word-")
-SYNSET_PT = Namespace("https://w3id.org/own-pt/wn30-pt/instances/synset-")
-WORDSENSE = Namespace("https://w3id.org/own-pt/wn30-pt/instances/wordsense-")
+from pyownpt.ownpt import CONTAINS_EXAMPLE, CONTAINS_GLOSS, CONTAINS_WORD, CONTAINS_WORDSENSE, OWNPT
+from pyownpt.ownpt import HAS_LABEL, SYNSETPT, SCHEMA
 
 
-def update_ownpt_from_dump(ownpt:Graph, wn:dict):
-    """"""
+class Update(OWNPT):
 
-    # removing WordSense from Synset
-    ownpt.remove((None, OWNPT.containsWordSense, None))
-    # removing properties of WordSense
-    ownpt.remove((None, RDF.type, OWNPT.WordSense))
-    ownpt.remove((None, OWNPT.wordNumber, None))
-    ownpt.remove((None, RDFS.label, None))
-    # removing Word from WordSense
-    ownpt.remove((None, OWNPT.word, None))
-    # removing properties of Word
-    ownpt.remove((None, RDF.type, OWNPT.Word))
-    ownpt.remove((None, OWNPT.lexicalForm, None))
+    def update(
+        self,
+        doc_suggestions = [],
+        doc_votes = [],
+        users_senior=[],
+        trashold_senior=1,
+        trashold_junior=2):
+        """"""
 
-    # removing gloss from from Synset
-    ownpt.remove((None, OWNPT.gloss, None))
-
-    # removing example from from Synset
-    ownpt.remove((None, OWNPT.example, None))
-
-    # removing antonymOf
-    ownpt.remove((None, OWNPT.antonymOf, None))
-    
-    # updates synsets
-    for synset in wn:
-        _update_synset(ownpt, synset)
-
-    # update relations
-    pointers_uri_map = {"wn30_pt_antonymOf":OWNPT.antonymOf}
-    _update_pointers(ownpt, wn, pointers_uri_map)
-
-    return ownpt
-
-
-def update_morpho_from_dump(wn:dict):
-    """"""
-    
-    ownpt = Graph()
-
-    # morphosemantic links
-    pointers_uri_map = {  
-        "wn30_pt_property": NOMLEX.property,
-        "wn30_pt_result": NOMLEX.result,
-        "wn30_pt_state": NOMLEX.state,
-        "wn30_pt_undergoer": NOMLEX.undergoer,
-        "wn30_pt_uses": NOMLEX.uses,
-        "wn30_pt_vehicle": NOMLEX.vehicle,
-        "wn30_pt_event": NOMLEX.event,
-        "wn30_pt_instrument": NOMLEX.instrument,
-        "wn30_pt_location": NOMLEX.location,
-        "wn30_pt_material": NOMLEX.material,
-        "wn30_pt_agent": NOMLEX.agent,
-        "wn30_pt_bodyPart": NOMLEX.bodyPart,
-        "wn30_pt_byMeansOf": NOMLEX.byMeansOf
-    }
-
-    # update links
-    _update_pointers(ownpt, wn, pointers_uri_map)
-    
-    return ownpt
-
-
-def dump_update(
-    doc_wn = [],
-    doc_suggestions = [],
-    doc_votes = [],
-    users_senior=[],
-    trashold_senior=1,
-    trashold_junior=2):
-    """"""
-
-    # acesses only sources
-    wn = [item["_source"] for item in doc_wn]
-    votes = [item["_source"] for item in doc_votes]
-    suggestions = [item["_source"] for item in doc_suggestions]
-
-    # filter suggestions
-    suggestions = _filter_suggestions(suggestions, votes, users_senior, trashold_senior, trashold_junior)
-
-    # joins synsets and suggestions
-    f_idl = lambda x: x["doc_id"]
-    f_idr = lambda x: x["doc_id"]
-    zipped = _left_zip_by_id(wn, suggestions, f_idl, f_idr)
-
-    # apply suggestions
-    for synset, suggestions in zipped:
-        _apply_suggestions(synset, suggestions)
-    
-    return doc_wn
-
-
-def _update_pointers(ownpt:Graph, synsets, pointer_uri_map:dict=dict()):
-    """"""
-
-    for pointer, predicate in pointer_uri_map.items():
-        _update_pointer(ownpt, synsets, pointer, predicate)
-
-
-def _update_pointer(ownpt:Graph, synsets, pointer_name:str, predicate:URIRef):
-    """"""
-
-    # join pairs
-    doc_id_map = {synset["doc_id"]:synset for synset in synsets}
-    pairs_sense_id = []
-    for source_synset in synsets:
-        if pointer_name not in source_synset:
-            continue
-        for pointer in source_synset[pointer_name]:
-            source_senses = _get_wordsenses(source_synset, pointer, "source_word")
-            
-            target_doc_id = pointer["target_synset"]
-            target_synset = doc_id_map[target_doc_id]
-            target_senses = _get_wordsenses(target_synset, pointer, "target_word")
-
-            pairs_sense_id += [(s, t) for s in source_senses for t in target_senses]
-    
-    # add relations
-    for source_sense_id, target_sense_id in pairs_sense_id:
-        source_wordsense = WORDSENSE[source_sense_id]
-        target_wordsense = WORDSENSE[target_sense_id]
-        ownpt.add((source_wordsense, predicate, target_wordsense))
-
-
-def _get_wordsenses(synset, pointer, key):
-    """"""
-    
-    doc_id = synset["doc_id"]
-
-    word = pointer[key] if key in pointer else None
-    words = synset["word_pt"] if "word_pt" in synset else []
-
-    # if not source/target word
-    if word is None:
-        return [f"{doc_id}-{i+1}" for i in range(len(words))]
-    # if source/target word in synset
-    if word in words:
-        return [f"{doc_id}-{1+words.index(word)}"]
-    # other cases
-    return []
-
-
-def _update_synset(ownpt:Graph, synset:dict):
-    """"""
-
-    doc_id = synset["doc_id"]
-    synset_pt = SYNSET_PT[doc_id]
-    
-    # adding word-pt
-    word_pt = synset["word_pt"] if "word_pt" in synset else []
-    for i, word in enumerate(word_pt, start=1):
-        word_pt = WORD[word.replace(" ", "_")]  
-        word_sense = WORDSENSE[f"{doc_id}-{i}"]
-
-        ownpt.add((synset_pt, OWNPT.containsWordSense, word_sense))
+        votes = [x["_source"] for x in doc_votes]
+        suggestions = [x["_source"] for x in doc_suggestions]
         
-        # word sense
-        ownpt.add((word_sense, RDF.type, OWNPT.WordSense))
-        ownpt.add((word_sense, OWNPT.wordNumber, Literal(i)))
-        ownpt.add((word_sense, RDFS.label, Literal(word, lang="pt")))
+        self.logger.info("formatting suggestions to apply")
+        
+        # filter suggestions
+        suggestions = self._filter_suggestions(
+                        suggestions, votes, users_senior,
+                        trashold_senior, trashold_junior)                
+        # sort results
+        suggestions = sorted(suggestions, key=lambda x:x["date"])
 
-        # word form
-        ownpt.add((word_sense, OWNPT.word, word_pt))
-        ownpt.add((word_pt, RDF.type, OWNPT.Word))
-        ownpt.add((word_pt, OWNPT.lexicalForm, Literal(word, lang="pt")))
+        # apply suggestions
+        self.logger.info("start applying suggestions")
+        self._apply_suggestions(suggestions)
 
-
-    # adding gloss-pt
-    gloss_pt = synset["gloss_pt"] if "gloss_pt" in synset else []
-    for i, gloss in enumerate(gloss_pt, start=1):
-        ownpt.add((synset_pt, OWNPT.gloss, Literal(gloss, lang="pt")))
-
-    # adding example-pt
-    example_pt = synset["example_pt"] if "example_pt" in synset else []
-    for i, example in enumerate(example_pt, start=1):
-        ownpt.add((synset_pt, OWNPT.example, Literal(example, lang="pt")))
+        # statistics
+        self.logger.info(f"total triples added by action: {self.added_triples}")
+        self.logger.info(f"total triples removed by action: {self.removed_triples}")
 
 
-def _apply_suggestions(synset:dict, suggestions:list):
-    for suggestion in suggestions:
-        _apply_suggestion(synset, suggestion)
+    def update_from_compare(self, report):
+        """"""
+
+        self.logger.info("formatting suggestions to apply")
+        suggestions = []
+        for doc_id, doc_report in report.items():
+            for action, params in doc_report["actions"].items():
+                for param in params:
+                    suggestions.append({"doc_id":doc_id,"params":param,"action":action})
+        
+        # apply suggestions
+        self.logger.info("start applying suggestions")
+        self._apply_suggestions(suggestions)
 
 
-def _apply_suggestion(synset:dict, suggestion):
-    action = suggestion["action"]
-    params = suggestion["params"]
 
-    if action == "add-word-pt":
-        _add_parameter(synset, "word_pt", params)
-    elif action == "add-gloss-pt":
-        _add_parameter(synset, "gloss_pt", params)
-    elif action == "add-example-pt":
-        _add_parameter(synset, "example_pt", params)
-    elif action == "remove-word-pt":
-        _remove_parameter(synset, "word_pt", params)
-    elif action == "remove-gloss-pt":
-        _remove_parameter(synset, "gloss_pt", params)
-    elif action == "remove-example-pt":
-        _remove_parameter(synset, "example_pt", params)
-    else:
-        print(f"Not a valid action: {action}")
-        # raise Exception(f"Not a valid action: {action}")
-
-    return synset
+    def _apply_suggestions(self, suggestions:list):
+        """"""
+        
+        for suggestion in tqdm.tqdm(suggestions):
+            self._apply_suggestion(suggestion)
 
 
-def _add_parameter(synset, key, params):
-    """"""
+    def _apply_suggestion(self, suggestion):
+        action = suggestion["action"]
+        params = suggestion["params"]
+        doc_id = suggestion["doc_id"]
+        synset = SYNSETPT[doc_id]
+        
+        result = True
 
-    if key in synset:
-        synset[key].append(params)
-    else:
-        synset[key] = [params]
+        if action == "add-word-pt":
+            # checks and adds suitable
+            item = self._get_sense(synset, params)
+            if item is not None:
+                result = False
+            else:
+                word = self._get_word(params, True)
+                sense = self._new_sense(synset, True)
+                label = self._new_lexical_literal(params)
+                self._add_triple((sense, HAS_LABEL, label), action)
+                self._add_triple((sense, CONTAINS_WORD, word), action)
+            
+        elif action == "add-gloss-pt":
+            # checks and adds suitable
+            item = self._get_gloss(synset, params)
+            if item is not None:
+                result = False
+            else:
+                item = self._new_lexical_literal(params, True)
+                self._add_triple((synset, CONTAINS_EXAMPLE, item), action)
 
+        elif action == "add-example-pt":
+            # checks and adds suitable
+            item = self._get_example(synset, params)
+            if item is not None:
+                result = False
+            else:
+                item = self._new_lexical_literal(params, True)
+                self._add_triple((synset, CONTAINS_EXAMPLE, item), action)
 
-def _remove_parameter(synset, key, params):
-    """"""
-
-    if key in synset:
-        if params in synset[key]:
-            synset[key].remove(params)
+        elif action == "remove-word-pt":
+            # finds and removes suitable
+            item = self._get_sense(synset, params)
+            if item is None:
+                result = False
+            else:
+                self._drop_triple((synset, CONTAINS_WORDSENSE, item), action)
+        
+        elif action == "remove-gloss-pt":
+            # finds and removes suitable
+            item = self._get_gloss(synset, params)
+            if item is None:
+                result = False
+            else:
+                self._drop_triple((synset, CONTAINS_GLOSS, item), action)
+            
+        elif action == "remove-example-pt":
+            # finds and removes suitable
+            item = self._get_example(synset, params)
+            if item is None:
+                result = False
+            else:
+                self._drop_triple((synset, CONTAINS_EXAMPLE, item), action)
+            
         else:
-            print(f"Param not in synset {synset['doc_id']} key {key}: {params} not in {synset[key]}")
-            # raise Exception(f"Param not in synset {synset['doc_id']} key {key}: {params}")
-    else:
-        print(f"Key not in synset: {key}")
-        # raise Exception(f"Key not in synset: {key}")
+            self.logger.warning(f"invalid action: {action}")
 
-
-def _filter_suggestions(
-    suggestions:list,
-    votes:list,
-    users_senior:list,
-    trashold_senior:int,
-    trashold_junior:int):
-    """"""
-
-    # joins suggestions and votes
-    f_idl = lambda x: x["id"]
-    f_idr = lambda x: x["suggestion_id"]
-    zipped = _left_zip_by_id(suggestions, votes, f_idl, f_idr)
-
-    # apply filter rules and return
-    return [x[0] for x in zipped if _rules(*x,users_senior,trashold_senior,trashold_junior)]
-
-
-def _rules(suggestion, votes:list, users_senior:list, trashold_senior:int, trashold_junior:int):
-    """"""
-
-    r1 = suggestion["status"] == "new"
-    r2 = suggestion["action"] != "comment"
-    score = sum([vote["value"] for vote in votes])
-    r3 = score >= trashold_senior and suggestion["user"] in users_senior or score >= trashold_junior
-
-    return all([r1,r2,r3])
-
-
-def _left_zip_by_id(listl, listr, f_idl, f_idr):
-    """"""
-
-    # review eficiency
-    zipped = {f_idl(l):{"l":l,"r":[]} for l in listl}
-    for itemr in listr:
-        _id = f_idr(itemr)
-        if _id in zipped:
-            zipped[_id]["r"].append(itemr)
+        # resulting
+        if result:
+            term = "added to" if action.startswith("add") else "removed from"
+            self.logger.debug(f"{action}: param '{params}' {term} '{synset.n3()}'")
         else:
-            print(f"Got invalid id to zip: {_id}")
-            # raise Exception(f"Got invalid id to zip: {_id}")
+            term = "already" if action.startswith("add") else "not"
+            self.logger.debug(f"{action}: param '{params}' {term} in '{synset.n3()}'")
 
-    return [(item["l"],item["r"]) for item in zipped.values()]
+
+    def _filter_suggestions(
+        self,
+        suggestions:list,
+        votes:list,
+        users_senior:list,
+        trashold_senior:int,
+        trashold_junior:int):
+        """"""
+
+        # joins suggestions and votes
+        f_idl = lambda x: x["id"]
+        f_idr = lambda x: x["suggestion_id"]
+        zipped = self._left_zip_by_id(suggestions, votes, f_idl, f_idr)
+
+        # apply filter rules and return
+        return [x[0] for x in zipped if self._rules(*x,users_senior,trashold_senior,trashold_junior)]
+
+
+    def _rules(
+        self,
+        suggestion,
+        votes:list,
+        users_senior:list,
+        trashold_senior:int,
+        trashold_junior:int):
+        """"""
+
+        
+        r1 = suggestion["status"] == "new"
+        # r1 = suggestion["status"] == "committed"
+        r2 = suggestion["action"] != "comment"
+        score = sum([vote["value"] for vote in votes])
+        r3 = score >= trashold_senior and suggestion["user"] in users_senior or score >= trashold_junior
+
+        return all([r1,r2,r3])
+
+
+    def _left_zip_by_id(self, listl, listr, f_idl, f_idr):
+        """"""
+
+        # review eficiency
+        zipped = {f_idl(l):{"l":l,"r":[]} for l in listl}
+        for itemr in listr:
+            _id = f_idr(itemr)
+            if _id in zipped:
+                zipped[_id]["r"].append(itemr)
+            else:
+                self.logger.debug(f"invalid id: {_id}")
+                # raise Exception(f"Got invalid id to zip: {_id}")
+
+        return [(item["l"],item["r"]) for item in zipped.values()]
