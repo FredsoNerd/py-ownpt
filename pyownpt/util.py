690fab6ce3e21bbe3bd10a6456c9acfec4078b24
FredsoNerd <fredsonerd@hotmail.com>
2021-07-01 17:20:51 -0300

    adding utils

---
+++ pyownpt/util.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from rdflib.util import guess_format
+
+def get_format(filepath:str):
+    """"""
+
+    filepath_format = guess_format(filepath, {"jsonld":"json-ld"})    
+    return filepath_format if filepath_format else filepath.split(".")[-1]
